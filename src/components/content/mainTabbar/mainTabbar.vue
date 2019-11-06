b55eac96c3c6afa4db5d430676f87b8ae11c6e89
zzw <827271993@qq.com>
2019-11-06 14:55:14 +0800

    提交修改

---
+++ src/components/content/mainTabbar/mainTabbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="blue">
+        <img slot="item-icon" src="~/assets/image/tabbar/Home.png" alt="">
+        <img slot="item-icon-active" src="~/assets/image/tabbar/Home.png" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="green">
+        <img slot="item-icon" src="~/assets/image/tabbar/购物车.png" alt="">
+        <img slot="item-icon-active" src="~/assets/image/tabbar/购物车.png" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="green">
+        <img slot="item-icon" src="~/assets/image/tabbar/购物车.png" alt="">
+        <img slot="item-icon-active" src="~/assets/image/tabbar/购物车.png" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~/assets/image/tabbar/我的.png" alt="">
+        <img slot="item-icon-active" src="~/assets/image/tabbar/我的.png" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from "../../common/tabbar/TabBar"
+  import TabBarItem from "../../common/tabbar/TabBarItem"
+  export default {
+    name: 'MainTabbar',
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+  @import "../../../assets/css/base.css";
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,202 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles ="['流行','新款','精选']" class="tab-control"/>
+    <goods-list :goods="goods['pop'].list" />
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>0</li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul> 
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar"
+  import HomeSwiper from "./childComps/HomeSwiper"
+  import RecommendView from "./childComps/HomeRecommendView"
+  import FeatureView from "./childComps/FeatureView"
+
+  import {getHomeMultidata, getHomeGoods} from "@/network/home.js"
+  import {Swiper, SwiperItem} from "components/common/swiper"
+  import TabControl from "components/content/tabControl/TabControl"
+  import GoodsList from "components/content/goods/GoodsList"
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList
+    },
+    data() {
+      return {
+        banner: [],
+        recommend: [],
+        goods: {
+          'pop': {'page':0, list: []},
+          'new': {'page':0, list: []},
+          'sell': {'page':0, list: []}
+        }
+      }
+    },
+    created() {
+      this.getHomeMultidata()
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banner = res.data.banner.list
+          this.recommend = res.data.recommend.list
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page++
+          console.log(res);
+          
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  #home {
+    padding-top: 44px;
+  }
+  .tab-control {
+    background-color: #fff;
+    position: sticky;
+    top: 44px;
+  }
+</style>
